1228476ba1238af05e7983141ff6289b0c4dcdb9
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-01-19 12:31:57 -0800

    can translate

--- components/translator.js
+++ components/translator.js
@@ -1,17 +1,31 @@
+const { trash } = require("./american-only.js");
 const americanOnly = require("./american-only.js");
 const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-class Translator {
+const terms = [];
+Object.keys(americanOnly).forEach(key => {
+    terms.push([key, americanOnly[key]])
+});
+Object.keys(americanToBritishSpelling).forEach(key => {
+    terms.push([key, americanToBritishSpelling[key]])
+});
+Object.keys(britishOnly).forEach(key => {
+    terms.push([britishOnly[key], key])
+})
 
+class Translator {
   americanToBritish(text) {
     let translation;
     var timeRegex = /(?<=\d)\:(?=\d{2})/g
     var titleRegex = /(?<=mr|mrs|ms|mx|dr|prof)\./gi
 
-    translation = text.replace(timeRegex, ".")
-    translation = text.replace(titleRegex, "")
+    translation = text.replace(timeRegex, ".").replace(titleRegex, "")
+
+    terms.forEach(term => {
+        translation = translation.replace(term[0], `<span class="highlight">${term[1]}</span>`)
+    });
 
     return translation;
   }
@@ -22,8 +36,11 @@ class Translator {
     var titleRegex = /(?<=mr|mrs|ms|mx|dr|prof)/gi
 
 
-    translation = text.replace(timeRegex, ":")
-    translation = text.replace(titleRegex, ".")
+    translation = text.replace(timeRegex, ":").replace(titleRegex, ".")
+
+    terms.forEach(term => {
+        translation = translation.replace(term[1], `<span class="highlight">${term[0]}</span>`)
+    });
     
     return translation;
   }
@@ -39,6 +56,7 @@ class Translator {
       translation = this.britishToAmerican(text);
     }
 
+    console.log(terms)
     return translation;
   }
 }
