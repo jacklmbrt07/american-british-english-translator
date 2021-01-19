5b2d771b5c6c452e6bc12dfe7c5a5f7795517e74
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-01-19 14:35:25 -0800

    understand the unit test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,35 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
+const locale = ["american-to-british", "british-to-american"];
+const spanRegex = /<\/?span[^>]*>/g;
 
-suite('Unit Tests', () => {
+suite("Unit Tests", () => {
+  suite("American to British english tests", () => {
+    test("Translate 'Mangoes are my favorite fruit.' to British English", (done) => {
+      assert.equal(
+        translator
+          .translate("Mangoes are my favorite fruit.", locale[0])
+          .replace(spanRegex, ""),
+        "Mangoes are my favourite fruit."
+      );
+      done();
+    });
 
+    test("Translate 'I ate yogurt for breakfast.' to British English", (done) => {
+      assert.equal(
+        translator
+          .translate("I ate yogurt for breakfast.", locale[0])
+          .replace(spanRegex, ""),
+        "I ate yoghurt for brekkie."
+      );
+      done();
+    });
+  });
+
+  suite("British to American english tests", () => {});
+
+  suite("Highlighted translation tests", () => {});
 });
