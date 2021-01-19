fdb139252587ff0ef2c72e8be209644e241759d5
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-01-19 14:08:48 -0800

    only unit and func tests left

--- components/translator.js
+++ components/translator.js
@@ -5,43 +5,73 @@ const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
 const terms = [];
-Object.keys(americanOnly).forEach(key => {
-    terms.push([key, americanOnly[key]])
+Object.keys(americanOnly).forEach((key) => {
+  terms.push([key, americanOnly[key]]);
 });
-Object.keys(americanToBritishSpelling).forEach(key => {
-    terms.push([key, americanToBritishSpelling[key]])
+Object.keys(americanToBritishSpelling).forEach((key) => {
+  terms.push([key, americanToBritishSpelling[key]]);
 });
-Object.keys(britishOnly).forEach(key => {
-    terms.push([britishOnly[key], key])
-})
+Object.keys(britishOnly).forEach((key) => {
+  terms.push([britishOnly[key], key]);
+});
+Object.keys(americanToBritishTitles).forEach((key) => {
+  terms.push([key, americanToBritishTitles[key]]);
+});
 
 class Translator {
   americanToBritish(text) {
-    let translation;
-    var timeRegex = /(?<=\d)\:(?=\d{2})/g
-    var titleRegex = /(?<=mr|mrs|ms|mx|dr|prof)\./gi
+    let translation = text;
 
-    translation = text.replace(timeRegex, ".").replace(titleRegex, "")
-
-    terms.forEach(term => {
-        translation = translation.replace(term[0], `<span class="highlight">${term[1]}</span>`)
+    terms.forEach((term) => {
+      translation = translation.replace(
+        term[0],
+        `<span class="highlight">${term[1]}</span>`
+      );
+      translation = translation.replace(
+        term[0].charAt(0).toUpperCase() + term[0].slice(1),
+        `<span class="highlight">${term[1].charAt(0).toUpperCase() + term[1].slice(1)}</span>`
+      );
     });
+
+    var timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]))/g;
+    let times = translation.match(timeRegex);
+    if (times) {
+      times.forEach((time) => {
+        translation = translation.replace(
+          time,
+          `<span class="highlight">${time.replace(":", ".")}</span>`
+        );
+      });
+    }
 
     return translation;
   }
 
   britishToAmerican(text) {
-    let translation;
-    var timeRegex = /(?<=\d)\.(?=\d{2})/g
-    var titleRegex = /(?<=mr|mrs|ms|mx|dr|prof)/gi
+    let translation = text;
 
-
-    translation = text.replace(timeRegex, ":").replace(titleRegex, ".")
-
-    terms.forEach(term => {
-        translation = translation.replace(term[1], `<span class="highlight">${term[0]}</span>`)
+    terms.forEach((term) => {
+      translation = translation.replace(
+        term[1],
+        `<span class="highlight">${term[0]}</span>`
+      );
+      translation = translation.replace(
+        term[1].charAt(0).toUpperCase() + term[1].slice(1),
+        `<span class="highlight">${term[0].charAt(0).toUpperCase() + term[0].slice(1)}</span>`
+      );
     });
-    
+
+    var timeRegex = /(([0-9]|0[0-9]|1[0-9]|2[0-3])(\.)([0-5][0-9]))/g;
+    let times = translation.match(timeRegex);
+    if (times) {
+      times.forEach((time) => {
+        translation = translation.replace(
+          time,
+          `<span class="highlight">${time.replace(".", ":")}</span>`
+        );
+      });
+    }
+
     return translation;
   }
 
@@ -56,8 +86,8 @@ class Translator {
       translation = this.britishToAmerican(text);
     }
 
-    if (translation === text){
-        translation = "Everything looks good to me!"
+    if (translation === text) {
+      translation = "Everything looks good to me!";
     }
 
     return translation;
